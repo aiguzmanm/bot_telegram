d63bfed46d9447d2f26b7539f3b49dc5981351ab
adguzman <adguzman@gmail.com>
2024-12-12 16:08:39 -0300

    sdsd

--- modules/telegram_utils.py
+++ modules/telegram_utils.py
@@ -29,9 +29,8 @@ def enviar_mensaje_telegram(mensaje, chat_id=None):
     rq.get(url, params=params)
 
 
-def enviar_reporte_telegram(fecha, base_dir):
+def enviar_reporte_telegram(fecha,base_dir = os.path.join(project_root, 'datos')):
     """Envía un mensaje de reporte parcial a Telegram"""
-    
     # Cargar configuración
     config = cargar_config()
     token = config['telegram']['token']
